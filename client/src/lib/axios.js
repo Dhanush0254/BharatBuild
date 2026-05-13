6a6ba5768c6785eb31cd2b32a55bef1e2738403d
Dhanush A <dhanush.a2424@gmail.com>
2026-05-14 03:50:09 +0530

    fix: render.yaml property and axios host protocol

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const baseURL = import.meta.env.VITE_API_URL 
-  ? `${import.meta.env.VITE_API_URL}/api/v1` 
+  ? `https://${import.meta.env.VITE_API_URL}/api/v1` 
   : '/api/v1';
 
 const api = axios.create({
